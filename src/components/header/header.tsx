44fc4763db51daaac7bf29465707c5ccf38b4a79
zhule <zhule@gaodun.com>
2021-04-18 23:09:55 +0800

    feat: 注销

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,48 @@
 
+// eslint-diabled-next-line
 // import { useHistory, useLocation, useParams, useRouteMatch, Link } from 'react-router-dom'
 import { useHistory, Link } from 'react-router-dom'
 import { Layout, Typography, Button, Input } from 'antd'
+import { useState, useEffect } from 'react'
+import jwt_decode, { JwtPayload as defaultJwtPayload } from 'jwt-decode'
+import { useSelector } from '../../redux/hooks'
+import { useDispatch } from 'react-redux'
+import { userSlice } from '../../redux/user/slice'
 // import styles from './header.moudle.css'
-export const Header: React.FC = (props) => {
 
-  // console.log('props', props)
+interface JwtPayload extends defaultJwtPayload {
+  username: string | null
+}
+
+
+export const Header: React.FC = (props) => {
   const history = useHistory(); // 导航操作
   // const location = useLocation(); // 当前路径的信息
   // const params = useParams(); // url中的参数
   // const match = useRouteMatch(); // 路径匹配的数据
+
+  const dispatch = useDispatch();
+  const jwt = useSelector(s => s.user.token);
+  // eslint-disable-next-line 
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    if (jwt) {
+      // eslint-disable-next-line 
+      const token = jwt_decode<JwtPayload>(jwt)
+      // setUsername(token.username)
+    }
+
+
+  }, [jwt])
+
+  // eslint-disable-next-line 
+  const onLogout = () => {
+    dispatch(userSlice.actions.logOut);
+    history.push('/');
+    window.location.reload(false);
+  }
+
   return (
     <>
       <Layout.Header>
