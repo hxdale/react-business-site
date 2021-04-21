7306efd214caebffe83dbf88c097d726df42648d
zhule <zhule@gaodun.com>
2021-04-21 13:40:34 +0800

    update

--- src/pages/test/testPage.tsx
+++ src/pages/test/testPage.tsx
@@ -1,7 +1,14 @@
+import { useState, useEffect } from 'react'
 export const TestPage = () => {
+    const [number,setNumber] = useState(0);
+    useEffect(() => {
+      console.log('===============')
+      document.title = `你点击了${number}次`;
+    }, [number]);
   return (
     <>
-      <h1>测试页面</h1>
+      <p>{number}</p>
+       <button onClick={()=>setNumber(number+1)}>+</button>
     </>
   )
 };
